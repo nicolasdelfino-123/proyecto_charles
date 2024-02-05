e18a8eabb5818785204d5420c332ff27411932bd
nicolasdelfino-123 <nicolasdelfino585@gmail.com>
2024-02-05 20:14:28 -0300

    html modified

--- app_charles/forms.py
+++ app_charles/forms.py
@@ -6,8 +6,13 @@ from django import forms
 
 # formulario busqueda
 class BuscarLibroForm(forms.Form):
-    termino_busqueda = forms.CharField(max_length=100, required=False, label='Buscar por Autor o Título del libro')
+    termino_busqueda = forms.CharField(
+    max_length=100,
+    required=False, 
+    label='Buscar por Autor o Título del libro',
+    widget=forms.TextInput(attrs={'placeholder': 'Ingresar título, autor o ISBN'})
+    )
   
-  
+
   
   
